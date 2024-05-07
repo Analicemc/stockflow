9a0c86c6603c18c9683400e435cef3d26ae09dd8
Analicemc <euanalicecosta@gmail.com>
2024-05-06 23:36:50 -0300

    Feat: classe de modelo de estoque

--- stockflow/models/Estoque.py
+++ stockflow/models/Estoque.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Estoque(models.Model):
     produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nro_lote   = models.CharField(max_length=100)
+    qtde_ativa = models.IntegerField()
--- stockflow/models/Produto.py
+++ stockflow/models/Produto.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.CharField
+    nome = models.CharField(max_length=100)
